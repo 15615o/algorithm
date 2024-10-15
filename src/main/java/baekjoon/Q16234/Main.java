d23c998b01784612dcd0dac9ff2d7ed778bbc732
15615o <kimsr042931@gmail.com>
2024-10-16 01:43:59 +0900

    BJ16234 완료

--- src/main/java/baekjoon/Q16234/Main.java
+++ src/main/java/baekjoon/Q16234/Main.java
@@ -6,14 +6,13 @@ package baekjoon.Q16234;
     - 연합 인구 총합 / 연합 국가 수를 연합 나라의 인구로 대체, 일 수++
     - 연합이 가능하지 않을 때까지 반복
     2. 시간복잡도
-    - O(N^2 * N^2) => 50^4 = 6,250,000 => 가능
+    - O(N^2 * 2000) => 2500 * 2000 = 5,000,000 => 가능
     3. 변수
     - int[][] map
-    - boolean[][] chk(지역변수)
-    - int people(지역변수)
-    - int countries(지역변수)
+    - boolean[][] chk
+    - int people
+    - int countries
     - int days
-    - Queue q
  */
 
 import java.util.*;
@@ -21,64 +20,49 @@ import java.io.*;
 
 public class Main {
     static int[][] map;
-    static boolean isMovable = true;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
     static boolean[][] chk;
-    static int days = 0;
 
-    static void bfs(int N, int L, int R){ // 연합이 여러개일 때 고려
-        int people = 0;
-        int countries = 0;
+    static boolean bfs(int N, int L, int R, int x, int y){ // 연합이 여러개일 때 고려
+        Queue<int[]> q = new LinkedList<>();
+        ArrayList<int[]> arr = new ArrayList<>();
+        q.add(new int[] {x, y});
+        arr.add(new int[] {x, y});
+        int people = map[y][x];
+        int countries = 1;
+        chk[y][x] = true;
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int nx = now[0];
+            int ny = now[1];
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++) {
-                for (int k = 0; k < 4; k++) {
-                    int ex = dx[k] + j;
-                    int ey = dy[k] + i;
+            for (int k = 0; k < 4; k++) {
+                int ex = dx[k] + nx;
+                int ey = dy[k] + ny;
 
-//                    System.out.println(Math.abs(map[ey][ex] - map[i][j]));
-                    if (ex >= 0 && ex < N && ey >= 0 && ey < N && (!chk[i][j] || !chk[ey][ex])
-                            && (L <= Math.abs(map[ey][ex] - map[i][j]) && Math.abs(map[ey][ex] - map[i][j]) <= R)) {
-                        System.out.println("map["+ey+"]["+ex+"] - map["+i+"]["+j+"] = " + Math.abs(map[ey][ex] - map[i][j]));
-                        System.out.println("map["+ey+"]["+ex+"] - map["+i+"]["+j+"] = " + (map[ey][ex] - map[i][j]));
-                        if(!chk[i][j]){ // 현재 국가가 체크되지 않았을 경우(중복합산 방지)
-                            people += map[i][j];
-                            countries++;
-                            chk[i][j] = true;
-                        }
-                        if(!chk[ey][ex]){ // 주변 국가가 체크되지 않았을 경우(중복합산 방지)
-                            people += map[ey][ex];
-                            countries++;
-                            chk[ey][ex] = true;
-                        }
-                    }
+                if (ex >= 0 && ex < N && ey >= 0 && ey < N && !chk[ey][ex]
+                    && (L <= Math.abs(map[ey][ex] - map[ny][nx]) && Math.abs(map[ey][ex] - map[ny][nx]) <= R)) {
+
+                    q.add(new int[] {ex, ey});
+                    arr.add(new int[] {ex, ey});
+                    people += map[ey][ex];
+                    countries++;
+                    chk[ey][ex] = true;
                 }
             }
         }
-        System.out.println();
 
-        if(countries > 0){
+        if(countries > 1){
             double avg = Math.floor(1.0 * people / countries);
-            System.out.println(people + ", " + countries);
-            for(int l=0; l<N; l++){
-                for(int m=0; m<N; m++){
-                    if(chk[l][m]) map[l][m] = (int) avg;
-                }
+            for(int[] coord : arr){
+                map[coord[1]][coord[0]] = (int) avg;
             }
-            days++;
-        } else {
-            isMovable = false;
+            return true;
         }
 
+        return false;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -89,6 +73,7 @@ public class Main {
         int L = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
         map = new int[N][N];
+        int days = 0;
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
@@ -97,10 +82,25 @@ public class Main {
             }
         }
 
-//        System.out.println(isMovable);
+        boolean isMovable = true;
         while(isMovable) {
+            isMovable = false;
+            boolean moved = false;
             chk = new boolean[N][N];
-            bfs(N, L, R);
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++) {
+                    if(!chk[i][j]){
+                        if(bfs(N, L, R, j, i)){
+                            moved = true;
+                        }
+
+                    }
+                }
+            }
+            if(moved){
+                isMovable = true;
+                days++;
+            }
         }
 
         bw.write(Integer.toString(days));
